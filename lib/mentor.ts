63984ae218b9794fffd051ec214d07a4ece6c201
JoshuaRalphz <solomonjoshua14040@gmail.com>
2024-05-30 00:42:57 +0800

    new icon

--- lib/mentor.ts
+++ lib/mentor.ts
@@ -5,6 +5,13 @@ export const isMentor = (userId?: string | null) => {
     process.env.NEXT_PUBLIC_MENTOR_ID_1,
     process.env.NEXT_PUBLIC_MENTOR_ID_2,
     process.env.NEXT_PUBLIC_MENTOR_ID_3,
+    process.env.NEXT_PUBLIC_MENTOR_ID_4,
+    process.env.NEXT_PUBLIC_MENTOR_ID_5,
+    process.env.NEXT_PUBLIC_MENTOR_ID_6,
+    process.env.NEXT_PUBLIC_MENTOR_ID_7,
+    process.env.NEXT_PUBLIC_MENTOR_ID_8,
+    process.env.NEXT_PUBLIC_MENTOR_ID_9,
+    process.env.NEXT_PUBLIC_MENTOR_ID_10,
     // Add more mentor IDs as needed
   ];
 
